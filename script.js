b573a726f0cc7b9ad24beb725f56922af9895f79
Avi <avka0001@stud.kea.dk>
2025-05-21 15:04:33 +0200

    script på popup

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ slider.addEventListener('input', () => {
       slide.classList.remove('active');
     }
   });
+}); 
+
+
+// POP-UP - AVI
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("infoCircle").style.display = "block";
+}
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("infoCircle").style.display = "none";
+}
+
+// Vis popup automatisk efter 4 sekunder
+window.addEventListener("load", function() {
+  setTimeout(function() {
+    document.getElementById("popup").style.display = "block";
+  }, 4000);
 });
+
+
+
