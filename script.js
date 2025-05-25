72fa644af35ac927a62bd2e4ce2d44c5eca0e32d
Ida <idla0004@stud.kea.dk>
2025-05-25 16:59:05 +0200

    Rettelser til medie siden

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-const slides = document.querySelectorAll('.slide');
-const slider = document.getElementById('sliderRange');
-
-slides[0].classList.add('active')
-
-slider.addEventListener('input', () => {
-  const index = parseInt(slider.value);
-
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.classList.add('active');
-    } else {
-      slide.classList.remove('active');
-    }
+document.querySelectorAll(".media-card").forEach((card) => {
+  card.addEventListener("mouseenter", () => {
+    console.log("Hover over:", card.querySelector("h4").innerText);
   });
-}); 
-
-
-
-
+});
