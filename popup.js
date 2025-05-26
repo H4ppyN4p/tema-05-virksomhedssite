68bc4e5d6706eba90484ee03dbbf510d871dcab3
Ida <idla0004@stud.kea.dk>
2025-05-26 10:48:22 +0200

    Rettet mobil version af medier

--- popup.js
+++ popup.js
@@ -2,75 +2,65 @@
 //Web3school doc for cookies; https://www.w3schools.com/js/js_cookies.asp
 //Web3school doc for Date() and GetTime(); https://www.w3schools.com/jsref/jsref_gettime.asp
 
-
-let cookies = document.cookie
+let cookies = document.cookie;
 //document.cookie = "lastName=; path=/; expires=Sun, 1 January 2020 12:00:00 UTC"
 
-
-let cookieExists = false
+let cookieExists = false;
 
 function checkCookie() {
-
   //Get a list of all current cookies on the websit
-  let cookielist = document.cookie.split("; ")
+  let cookielist = document.cookie.split("; ");
 
   //for each cookie...
-  cookielist.forEach(element => {
-
+  cookielist.forEach((element) => {
     //split it between it's key and value pair
-    let word = element.split("=")
+    let word = element.split("=");
 
     //check if the key for "popup" is exist
-    if (word[0] == "popup"){
-
+    if (word[0] == "popup") {
       //if it does, set the value for the cookie existing to true
-      cookieExists = true
+      cookieExists = true;
     }
   });
 
   //then if the cookie exist
-  if (cookieExists){
-
+  if (cookieExists) {
     //console.log("it exists")
 
     //Set the popup element to be displayed closed
     document.getElementById("popup").style.display = "none";
     document.getElementById("infoCircle").style.display = "block";
-    
-  //else, create the cookie
+
+    //else, create the cookie
   } else {
-    console.log("Cookie doesnt exist - creating cookie")
+    console.log("Cookie doesnt exist - creating cookie");
 
     //Create a new Date() constant to get the date
-    const d = new Date()
+    const d = new Date();
 
     //Get the date + 1 hour
-    d.setTime(d.getTime() + (60*60*1000));
+    d.setTime(d.getTime() + 60 * 60 * 1000);
 
     //Set the cookie expiration date
-    let expires = "expires="+ d
+    let expires = "expires=" + d;
 
     //Create new cookie with an expiration date 1 hour in the future
-    document.cookie = "popup=true;" + expires + ";path=/" 
+    document.cookie = "popup=true;" + expires + ";path=/";
 
     //Then make the popup run
     // Vis popup automatisk efter 4 sekunder
-    window.addEventListener("load", function() {
-      setTimeout(function() {
-        console.log("running function")
+    window.addEventListener("load", function () {
+      setTimeout(function () {
+        console.log("running function");
         document.getElementById("popup").style.display = "block";
       }, 4000);
     });
-   
   }
-  
 }
 
-checkCookie()
+checkCookie();
 
 if (cookieExists) {
-
-  
 }
 
 // POP-UP - AVI
@@ -78,7 +68,6 @@ if (cookieExists) {
 function closePopup() {
   document.getElementById("popup").style.display = "none";
   document.getElementById("infoCircle").style.display = "block";
-  
 }
 
 function openPopup() {
@@ -86,10 +75,6 @@ function openPopup() {
   document.getElementById("infoCircle").style.display = "none";
 }
 
-
-
-
 function toggleMenu() {
-    document.querySelector(".mobile-menu").classList.toggle("active");
+  document.querySelector(".mobile-menu").classList.toggle("active");
 }
-
